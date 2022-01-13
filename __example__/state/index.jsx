adf557257d787eae28fa7f3d7cfbdfccd1069e45
Jonti Hudson <33636074+JontiHudson@users.noreply.github.com>
2022-01-13 09:07:18 +0000

    1.6.0-beta46

--- __example__/state/index.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DemoState = void 0;
-const shared_state_1 = require("@huds0n/shared-state");
-exports.DemoState = new shared_state_1.SharedState({
-    screen: 'WELCOME',
-});
--- src/AnimationSheet.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AttachSheet = exports.AnimationSheet = void 0;
-exports.AnimationSheet = {
-    create: (animation) => animation,
-};
-exports.AttachSheet = {
-    create: (animation) => animation,
-};
